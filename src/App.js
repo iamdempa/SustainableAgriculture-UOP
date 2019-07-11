4746c7b246d3f09ae71786b26f02f23afd8557cb
Jananath Banuka Jayarathna <32482769+iamdempa@users.noreply.github.com>
2019-07-11 23:50:32 +0530

    4th

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 export default class App extends Component{
   render(){
     return(
-      <div>
-        <h1>Hi</h1>
-      </div>
+      <Router>
+        
+      </Router>
     );
   }
 }
