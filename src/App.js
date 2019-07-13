1f17751a430ba601ce9fb23eb9cf15fe15bfd895
Jananath Banuka Jayarathna <32482769+iamdempa@users.noreply.github.com>
2019-07-13 12:25:15 +0530

    7th

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.css';
+
 
 export default class App extends Component{
   render(){
